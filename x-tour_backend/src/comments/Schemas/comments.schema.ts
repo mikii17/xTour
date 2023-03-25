525ed91115cadab2bdcc480c8182b50f2498623e
Orion777-cmd <abiy.biru78@gmail.com>
2023-03-25 14:45:44 +0300

    add the schema of comments feature

--- x-tour_backend/src/comments/Schemas/comments.schema.ts
+++ x-tour_backend/src/comments/Schemas/comments.schema.ts
@@ -1,3 +1,15 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import * as mongoose from 'mongoose';
 
+@Schema()
+export class Comments{
+    @Prop({required: true})
+    _id: String;
+
+    @Prop()
+    message:String;
+
+    @Prop({ref:"Users", type:[mongoose.Schema.Types.ObjectId]})
+    replies:mongoose.Schema.Types.ObjectId[];
+}
