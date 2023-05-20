818914cbe2cceb749767a415e2671820ac8c09be
dani7514 <106800350+dani7514@users.noreply.github.com>
2023-05-20 16:16:02 +0300

    Change ref

--- x-tour_backend/src/comments/Schemas/comments.schema.ts
+++ x-tour_backend/src/comments/Schemas/comments.schema.ts
@@ -1,18 +1,19 @@
 import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
 import mongoose from 'mongoose';
+import { User } from './userSchema/user.schema';
 
 @Schema({
     timestamps: true
 })
 export class Comments{
-    @Prop({required: true, ref: "Users", type: mongoose.Types.ObjectId})
-    commenterId: mongoose.Types.ObjectId;
+    @Prop({required: true, ref: "User", type: mongoose.Schema.Types.ObjectId})
+    commenterId: User;
     
-    @Prop({ref: 'Comments', type: mongoose.Types.ObjectId})
-    replyId: mongoose.Types.ObjectId;
+    @Prop({ref: 'Comments', type: mongoose.Schema.Types.ObjectId})
+    replyId: Comments;
 
-    @Prop()
-    postId: String;
+    @Prop({ref: 'Post', type: mongoose.Schema.Types.ObjectId})
+    postId: Post ;
 
     @Prop()
     message:String;
